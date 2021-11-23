e41aa3d71b3d8794dea72ff8a1f35fa8423f46df
娜一抹惊鸿 <18610649031@163.com>
2021-11-23 15:00:09 +0800

    feat: 配置proxy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -7,11 +8,19 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { zh_CN } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { IUrlConfig } from './model/res';
 
 registerLocaleData(zh);
+
+const UrlConfig: IUrlConfig = {
+  url: '/api'
+}
 
 @NgModule({
   declarations: [
@@ -21,10 +30,24 @@ registerLocaleData(zh);
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NzFormModule,
+    NzInputModule,
+    NzButtonModule,
+    NzCheckboxModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  providers: [
+    {
+      provide: NZ_I18N,
+      useValue: zh_CN
+    },
+    {
+      provide: 'URL_CONFIG',
+      useValue: UrlConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
